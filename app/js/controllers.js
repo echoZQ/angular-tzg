4397403c5d6c9b71a0dd6b027784a61a66a2346f
echoZQ <453255811@qq.com>
2015-04-22 14:17:07 +0800

    update result

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -37,3 +37,71 @@ tzgControllers.controller('BattleTzgCtrl', ['$scope', function($scope) {
         $scope.showPkingFlag = true;
     };
 }]);
+
+/**
+ * 偷袭及战绩历史
+ */
+tzgControllers.controller('HistoryListCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.rankListShowFlag = true;
+    $scope.changeRank = function (str) {
+        var arr = ['btn-battle', 'btn-attack'],
+            index = 0,
+            length = arr.length - 1,
+            target,
+            el;
+
+        for (var i = 0; i <= length; i++) {
+            if (arr[i].indexOf(str) > 0) {
+                index = i;
+                break;
+            }
+        }
+
+        $scope.rankListShowFlag = index == 0 ? true : false;
+        target = document.querySelector("." + arr[index]);
+        if (target.className.indexOf("chosed") > 0) {
+            return;
+        } else {
+            target.className += " chosed";
+            el = document.querySelector("." + arr[length - index]);
+            el.className = el.className.replace(/ chosed/, "");
+        }
+
+    };
+
+    //获取历史记录
+    $http.get('../app/json/historyList.json').
+        success(function (data, status) {
+            if (status == 200) {
+                $scope.attackList = data.attackList;
+                $scope.battleList = data.battleList;
+            }
+        }
+    );
+
+    //获取pk结果
+    $http.get('../app/json/historyList.json').
+        success(function (data, status) {
+            if (status == 200) {
+                $scope.attackList = data.attackList;
+                $scope.battleList = data.battleList;
+            }
+        }
+    );
+}]);
+
+/**
+ * pk结果
+ */
+tzgControllers.controller("PkResultCtrl", ['$scope', '$http', function($scope, $http) {
+    $http.get('../app/json/pkResult.json').
+        success(function (data, status) {
+            if (status == 200) {
+                $scope.win = data.win;
+                $scope.flow = data.flow;
+                $scope.fromUser = data.fromUser;
+                $scope.targetUser = data.targetUser;
+            }
+        }
+    );
+}]);
